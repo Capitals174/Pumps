63650d600120f165c1af9adc7ce34aed60ecc1e0
Aleksandr.Varkentin@evraz.com <Aleksandr.Varkentin@evraz.com>
2021-12-24 15:59:10 +0300

    change the structure

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from capacitor import capacitor
 
-tes = capacitor(G_steam=150, p_n=1.2, p_k=1.1, t_h=20, p_sk=0.1, pp=130)
+tes = capacitor(g_steam=69, p_n=0.72, p_k=0.51, t_h=20, p_sk=0.03, p_steam=130)
 print(tes)
 
 
